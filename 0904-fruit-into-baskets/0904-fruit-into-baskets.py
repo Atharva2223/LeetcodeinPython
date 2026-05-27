9fae4d8b9f59a4ae4e22a36a3c88c210ea6a5008
Atharva Patade <95520111+Atharva2223@users.noreply.github.com>
2026-05-26 20:15:48 -0700

    Time: 165 ms (85.94%), Space: 25.3 MB (92.84%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -12,17 +12,15 @@ class Solution:
 
             my_dict[fruits[right]] = my_dict.get(fruits[right],0)+1
 
-            while len(my_dict) > 2:
+            if len(my_dict) > 2:
 
                 my_dict[fruits[left]] -= 1
 
                 if my_dict[fruits[left]] == 0:
                     del my_dict[fruits[left]]
                 left+=1
-                
-               
-
-            max_length =  max(max_length,right-left+1)
+            if len(my_dict) <= 2:
+                max_length =  max(max_length,right-left+1)
             right+=1
         return max_length
 
