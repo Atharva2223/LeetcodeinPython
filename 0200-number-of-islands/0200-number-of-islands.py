6da94356c6ff6139a2c6ef36a574da1ce1d6c50a
Atharva Patade <95520111+Atharva2223@users.noreply.github.com>
2026-06-10 14:31:10 -0700

    Time: 255 ms (36.16%), Space: 21.4 MB (92.04%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,47 +1,35 @@
 class Solution:
-
-    def dfs(self,visited,grid,r,c):
-
+    def bfs(self,grid,visited,i,j):
         rows = len(grid)
         cols = len(grid[0])
-
-
         queue = deque()
-        queue.append((r,c))
-        visited[r][c] = 1
 
-        while len(queue)!=0:
-            i,j = queue.popleft()
+        queue.append((i,j))
 
-            for x,y in [(-1,0),(1,0),(0,1),(0,-1)]:
-                new_x,new_y = x+i,y+j
+        while len(queue) !=0:
+            x,y = queue.popleft()
+            for xx, yy in ([0,-1],[0,1],[-1,0],[1,0]):
+                new_x, new_y = x+xx, y+yy
 
-                if new_x <0 or new_y <0 or new_x >= rows or new_y >= cols:
+                if new_x < 0 or new_x >= rows or new_y < 0 or new_y >=cols:
                     continue
-                if grid[new_x][new_y]  == '0':
+                if grid[new_x][new_y] == "0":
                     continue
                 if visited[new_x][new_y] == 1:
                     continue
+                visited[new_x][new_y] = 1
                 queue.append((new_x,new_y))
-                visited[new_x][new_y] =1
-
 
     def numIslands(self, grid: List[List[str]]) -> int:
+
+        count = 0
         rows = len(grid)
         cols = len(grid[0])
-        count = 0
 
         visited = [[0 for _ in range(cols)] for _ in range(rows)]
-
         for r in range(rows):
             for c in range(cols):
-
                 if grid[r][c] == "1" and visited[r][c] == 0:
                     count+=1
-                    self.dfs(visited,grid,r,c)
+                    self.bfs(grid,visited,r,c)
         return count
-
-
-
-
-        
